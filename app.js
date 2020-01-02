4abff9eee95480377cdc0910e9d84a258aa224fe
JonTryggvi <jontryggvi@jontryggvi.is>
2020-01-02 17:28:19 +0100

    firsrt

---
+++ app.js
@@ -0,0 +1,118 @@
+import { moves, BLOCK_SIZE, KEY, LEVEL, account , time} from './js/constants';
+import {Board, POINTS} from './js/board'
+// import Piece from './js/Piece'
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+const canvasNext = document.getElementById('next');
+const ctxNext = canvasNext.getContext('2d');
+let requestId;
+let board = new Board(ctx, ctxNext);
+addEventListener();
+initNext();
+
+function initNext() {
+  // Calculate size of canvas from constants.
+  ctxNext.canvas.width = 4 * BLOCK_SIZE;
+  ctxNext.canvas.height = 4 * BLOCK_SIZE;
+  ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function addEventListener() {
+  document.addEventListener('keydown', handleEvent);
+  document.addEventListener('click', handleEvent);
+}
+function handleEvent(event) {
+  let keyCode = event.keyCode || event.target.dataset.keyCode
+  console.log(event.keyCode)
+  if (keyCode === KEY.P) {
+    pause();
+  }
+  if (keyCode === KEY.ESC) {
+    gameOver();
+  } else if (moves[keyCode]) {
+    event.preventDefault();
+    // Get new state
+    let p = moves[keyCode](board.piece, board);
+    if (keyCode === KEY.SPACE) {
+      // Hard drop
+      while (board.valid(p)) {
+        account.score += POINTS.HARD_DROP;
+        board.piece.move(p);
+        p = moves[KEY.DOWN](board.piece);
+      }
+      board.piece.hardDrop();     
+    } else if (board.valid(p)) {
+      board.piece.move(p);
+      if (keyCode === KEY.DOWN) {
+        account.score += POINTS.SOFT_DROP;         
+      }
+    }
+  }
+}
+
+function resetGame() {
+  account.score = 0;
+  account.lines = 0;
+  account.level = 0;
+  board.reset();
+  time.start = 0;
+  time.elapsed = 0;
+  time.level = LEVEL[account.level];
+}
+
+function play() {
+  resetGame();
+  time.start = performance.now();
+  // If we have an old game running a game then cancel the old
+  if (requestId) {
+    cancelAnimationFrame(requestId);
+  }
+
+  animate();
+}
+
+function animate(now = 0) {
+  time.elapsed = now - time.start;
+  if (time.elapsed > time.level) {
+    time.start = now;
+    if (!board.drop()) {
+      gameOver();
+      return;
+    }
+  }
+
+  // Clear board before drawing new state.
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  board.draw();
+  requestId = requestAnimationFrame(animate);
+}
+
+function gameOver() {
+  cancelAnimationFrame(requestId);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(1, 3, 8, 1.2);
+  ctx.font = '1px Arial';
+  ctx.fillStyle = 'red';
+  ctx.fillText('GAME OVER', 1.8, 4);
+}
+
+function pause() {
+  if (!requestId) {
+    animate();
+    return;
+  }
+
+  cancelAnimationFrame(requestId);
+  requestId = null;
+  
+  ctx.fillStyle = 'black';
+  ctx.fillRect(1, 3, 8, 1.2);
+  ctx.font = '1px Arial';
+  ctx.fillStyle = 'yellow';
+  ctx.fillText('PAUSED', 3, 4);
+}
+
+const btn = document.querySelector('.play-button')
+btn.addEventListener('click', play);
+
---
+++ js/constants.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+const LINES_PER_LEVEL = 10;
+const COLORS = [
+  'none',
+  'cyan',
+  'midnightblue',
+  'darkorange',
+  'khaki',
+  'seagreen',
+  'darkviolet',
+  'tomato'
+];
+Object.freeze(COLORS);
+
+const SHAPES = [
+  [],
+  [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+  [[2, 0, 0], [2, 2, 2], [0, 0, 0]],
+  [[0, 0, 3], // 0,0 -> 2,0 ; 0,1 -> 1,0 ; 0,2 -> 0,0
+   [3, 3, 3], // 1,0 -> 2,1 ; 1,1 -> 1,1 ; 1,2 -> 0,1 
+   [0, 0, 0]],// 2,0 -> 2,2 ; 2,1 -> 1,2 ; 2,2 -> 0,2
+  [[4, 4], [4, 4]],
+  [[0, 5, 5], [5, 5, 0], [0, 0, 0]],
+  [[0, 6, 0], [6, 6, 6], [0, 0, 0]],
+  [[7, 7, 0], [0, 7, 7], [0, 0, 0]]
+];
+Object.freeze(SHAPES);
+
+const KEY = {
+  ESC: 27,
+  SPACE: 32,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  P: 80,
+  Q: 81
+}
+Object.freeze(KEY);
+
+const POINTS = {
+  SINGLE: 100,
+  DOUBLE: 300,
+  TRIPLE: 500,
+  TETRIS: 800,
+  SOFT_DROP: 1,
+  HARD_DROP: 2,
+}
+Object.freeze(POINTS);
+
+const LEVEL = {
+  0: 800,
+  1: 720,
+  2: 630,
+  3: 550,
+  4: 470,
+  5: 380,
+  6: 300,
+  7: 220,
+  8: 130,
+  9: 100,
+  10: 80,
+  11: 80,
+  12: 80,
+  13: 70,
+  14: 70,
+  15: 70,
+  16: 50,
+  17: 50,
+  18: 50,
+  19: 30,
+  20: 30,
+  // 29+ is 20ms
+}
+Object.freeze(LEVEL);
+
+const ROTATION = {
+  LEFT: 'left',
+  RIGHT: 'right'
+}
+Object.freeze(ROTATION);
+
+let moves = {
+  [KEY.LEFT]: p => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWN]: p => ({ ...p, y: p.y + 1 }),
+  [KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
+  [KEY.UP]: (p, board) => board.rotate(p, ROTATION.RIGHT),
+  [KEY.Q]: (p, board) => board.rotate(p, ROTATION.LEFT)
+};
+
+
+let accountValues = {
+  score: 0,
+  level: 0,
+  lines: 0
+}
+let account = new Proxy(accountValues, {
+  set: (target, key, value) => {
+    target[key] = value;
+    updateAccount(key, value);
+    return true;
+  }
+});
+
+
+function updateAccount(key, value) {
+  let element = document.getElementById(key);
+  if (element) {
+    element.textContent = value;
+  }
+}
+
+let time = { start: 0, elapsed: 0, level: 0 }
+
+module.exports = { COLS, ROWS, BLOCK_SIZE, KEY, LINES_PER_LEVEL, SHAPES, POINTS, LEVEL, ROTATION, COLORS, moves, account, time }
